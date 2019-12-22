18eb505ef342aa7276b2b7fa1a59d54223c0bfc0
maainul <mainul080@yahoo.com>
2019-12-22 20:56:11 +0600

    upper and lower case java

---
+++ src/javastrings/UpperCase.java
@@ -0,0 +1,13 @@
+package javastrings;
+
+public class UpperCase {
+
+	public static void main(String[] args) {
+		String s1 = "Mainul";
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+		System.out.println(s1);
+
+	}
+
+}
