e708c37a904b8e2e35b96cdf58f4febadc68dd36
maainul <mainul080@yahoo.com>
2019-12-22 21:16:59 +0600

    string charat

---
+++ src/javastrings/CharAt.java
@@ -0,0 +1,17 @@
+package javastrings;
+
+public class CharAt {
+
+	public static void main(String[] args) {
+		String s1 = "Mainul";
+		System.out.println(s1.charAt(0));
+		System.out.println(s1.charAt(3));
+		for (int i = 0; i < s1.length(); i++) {
+			System.out.println(s1.charAt(i));
+		}
+		
+		System.out.println(s1.length());
+
+	}
+
+}
