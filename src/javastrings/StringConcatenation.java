e306b0c0dd7e98e7e872519d0c4f0f9faea82f5d
maainul <mainul080@yahoo.com>
2019-12-22 18:32:44 +0600

    string concatenation

---
+++ src/javastrings/StringConcatenation.java
@@ -0,0 +1,17 @@
+package javastrings;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		String s1 = "Mainul";
+		String s2 = "Mainul";
+		String s3 = "Hasan";
+		// by + method
+		System.out.println(s1 +" "+ s3);
+		// by concat()
+		System.out.println(s1.concat(s2)); // without space
+		System.out.println(s1.concat(" ").concat(s3));
+		
+	}
+
+}
