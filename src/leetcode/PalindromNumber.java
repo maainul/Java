efb0314227895a14df23192745461ea89238bde8
maainul <mainul080@yahoo.com>
2019-12-22 14:46:07 +0600

    first commit

---
+++ src/leetcode/PalindromNumber.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+class Number{
+	 int x;
+	 public void reverseInteger(int x) {	        
+	        long rev = 0;
+	        long n = x;
+	        
+	        while(n > 0){
+	            rev = (rev*10)+(n%10);
+	            n = n/10;
+	        }
+	        
+	        
+	        if(x == rev) {
+	        	System.out.println(rev);
+	        	System.out.println("palindrom"); 	
+
+	        }
+	        else {
+				System.out.println("Not a palindrom");
+			}
+	    }
+}
+
+
+
+public class PalindromNumber {
+
+	public static void main(String args[]) {
+       Number number = new Number();
+       number.reverseInteger(00100);
+    }
+   }
+ 
+
+
