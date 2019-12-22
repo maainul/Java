cce1467e04dfa76154f525f60d4da77a6c88fbc4
maainul <mainul080@yahoo.com>
2019-12-22 20:59:40 +0600

    trim method

---
+++ src/javastrings/TrimMethod.java
@@ -0,0 +1,13 @@
+package javastrings;
+
+public class TrimMethod {
+
+	public static void main(String[] args) {
+		String s1 = "  Mainul  ";
+		System.out.println(s1);
+		//The string trim() method eliminates white spaces before and after string.
+		System.out.println(s1.trim()); 
+
+	}
+
+}
