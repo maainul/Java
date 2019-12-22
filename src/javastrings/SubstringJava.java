4a8e19616c733b5f2cb8efb275f9b2fe3a68fc95
maainul <mainul080@yahoo.com>
2019-12-22 18:38:46 +0600

    substring methods

---
+++ src/javastrings/SubstringJava.java
@@ -0,0 +1,17 @@
+package javastrings;
+
+public class SubstringJava {
+
+	public static void main(String[] args) {
+		String s1 = "Mainul";
+		String s2 = "MainulHasan";
+		String s3 = "Hasan";
+
+		// it print index to rest of all// given index to last index
+		System.out.println(s1.substring(3)); 
+		// it print given first to given last.
+		// suppose (1,5) it will print 2,3,4,5 value
+		System.out.println(s2.substring(1, 5));
+	}
+
+}
