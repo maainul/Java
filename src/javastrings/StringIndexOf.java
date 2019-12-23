4b92f9d1e888ec4b010c884847a7575478c3c033
maainul <mainul080@yahoo.com>
2019-12-23 19:50:28 +0600

    index of

---
+++ src/javastrings/StringIndexOf.java
@@ -0,0 +1,13 @@
+package javastrings;
+
+public class StringIndexOf {
+
+	public static void main(String[] args) {
+		String s1 = "I am from Bangladesh.";
+		System.out.println(s1.indexOf("from"));
+		System.out.println(s1.indexOf('I'));
+		System.out.println(s1.indexOf("Ban"));
+
+	}
+
+}
