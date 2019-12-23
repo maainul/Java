11eb080ca252f58d8b107b6fb156711db933d6d6
maainul <mainul080@yahoo.com>
2019-12-23 20:31:49 +0600

    rename replace method

--- src/javastrings/StringReplaceMethod.java
+++ src/javastrings/StringReplaceMethod.java
@@ -1,6 +1,6 @@
 package javastrings;
 
-public class ReplaceMethod {
+public class StringReplaceMethod {
 
 	public static void main(String[] args) {
 		String s1 = "Java is a programming language.Java is a iceland";
@@ -8,5 +8,12 @@ public class ReplaceMethod {
 		String replaceString = s1.replace("Java", "Kava");
 		System.out.println(replaceString);
 		System.out.println(replaceString.replace("Kava", "Java"));
+		System.out.println(s1.replace("a", "A"));
 	}
 }
+/*
+ *  Java is a programming language.Java is a iceland
+ *  Kava is a programming language.Kava is a iceland 
+ *  Java is a programming language.Java is a iceland
+ *  JAvA is A progrAmming lAnguAge.JAvA is A icelAnd
+ */
