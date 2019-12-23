c66f184a2ba6f9147094c52cf774508519b1fb07
maainul <mainul080@yahoo.com>
2019-12-23 19:53:36 +0600

    rename

--- src/javastrings/StringStartsAndEndWith.java
+++ src/javastrings/StringStartsAndEndWith.java
@@ -1,6 +1,6 @@
 package javastrings;
 
-public class StartsAndEndWith {
+public class StringStartsAndEndWith {
 
 	public static void main(String[] args) {
 		String s1 = "Mainul";
@@ -9,6 +9,12 @@ public class StartsAndEndWith {
 		System.out.println(s1.startsWith("M"));
 		System.out.println(s1.endsWith("l"));
 		System.out.println(s1.endsWith("L"));
+		if(s1.endsWith("me")) {
+			System.out.println("You know me");
+		}
+		else {
+			System.out.println("You don't know me");
+		}
 	}
 
 }
