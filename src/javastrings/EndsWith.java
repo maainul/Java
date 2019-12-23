e293d5100fb6555c58be0b65dc774938cb02260d
maainul <mainul080@yahoo.com>
2019-12-23 19:09:57 +0600

    string ends with

---
+++ src/javastrings/EndsWith.java
@@ -0,0 +1,19 @@
+package javastrings;
+
+public class EndsWith {
+
+	public static void main(String[] args) {
+		String nameString = "What do you know about me";
+		System.out.println(nameString.endsWith("me"));
+		System.out.println(nameString.endsWith("d"));
+		if(nameString.endsWith("me")) {
+			System.out.println("You know me");
+		}
+		else {
+			System.out.println("You don't know me");
+		}
+		
+
+	}
+
+}
