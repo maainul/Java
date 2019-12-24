6415e69e52c2e584b8855261177df5fe5f633506
maainul <mainul080@yahoo.com>
2019-12-24 20:21:50 +0600

    copystring method

---
+++ src/javastrings/FAQconvertStringToInt.java
@@ -0,0 +1,22 @@
+package javastrings;
+
+public class FAQconvertStringToInt {
+
+	public static void main(String[] args) {
+		String string = "200";
+		
+		// Integer.parseInt()
+		int i = Integer.parseInt(string);
+		System.out.println(i);
+		//Understanding String Concatenation Operator
+		System.out.println(string+100);//200100, because "200"+100, here + is a string concatenation
+		System.out.println(i+100);//300, because 200+100, here + is a binary plus operator  
+		
+		// Integer.valueOf()
+		//converting String into Integer using Integer.valueOf() method  
+		Integer integer = Integer.valueOf(string);
+		System.out.println(integer);
+
+	}
+
+}
