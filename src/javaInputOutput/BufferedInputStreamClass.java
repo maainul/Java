22a4739f3a934d7db412098d9fd89b3585ade9a5
maainul <mainul080@yahoo.com>
2019-12-29 05:52:49 +0600

    buffered input stream

--- src/javaInputOutput/BufferedInputStreamClass.java
+++ src/javaInputOutput/BufferedInputStreamClass.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class BufferedInputStreamClass{    
  public static void main(String args[]){    
   try{    
-    FileInputStream fin=new FileInputStream("D:\\testout.txt");    
+    FileInputStream fin=new FileInputStream("D:\\testout1.txt");    
     BufferedInputStream bin=new BufferedInputStream(fin);    
     int i;    
     while((i=bin.read())!=-1){    
