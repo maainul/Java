4c2e11dc9a7f482cba201b47143b5113f0054623
maainul <mainul080@yahoo.com>
2019-12-28 20:35:48 +0600

    create java input output

---
+++ src/javaInputOutput/JavaFileOutputStreamExample1writebyte.java
@@ -0,0 +1,15 @@
+package javaInputOutput;
+
+import java.io.FileOutputStream;
+
+public class JavaFileOutputStreamExample1writebyte {
+    public static void main(String args[]){    
+        try{    
+          FileOutputStream fout=new FileOutputStream("C:\\Users\\Md.Mainul hasan anik\\gitrepo\\Java\\src\\javaInputOutput\\createdfiles\\file.txt");    
+          fout.write(65);   
+          fout.write(112);
+          fout.close();    
+          System.out.println("success...");    
+         }catch(Exception e){System.out.println(e);}    
+   }
+}
