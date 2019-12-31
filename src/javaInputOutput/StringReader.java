49b8da8bdeb7917b24ee721b302137284d8f6aa5
maainul <mainul080@yahoo.com>
2019-12-31 12:38:55 +0600

    create stringreader class

--- src/javaInputOutput/StringReader.java
+++ src/javaInputOutput/StringReader.java
@@ -3,7 +3,11 @@ package javaInputOutput;
 public class StringReader {
 
 	public static void main(String[] args) {
-		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 
