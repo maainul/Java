eebb8608e00cb5aca06d786f277464bc83b87284
maainul <mainul080@yahoo.com>
2019-12-31 13:15:15 +0600

    file reader

---
+++ src/javaInputOutput/FileReader.java
@@ -0,0 +1,20 @@
+package javaInputOutput;
+
+public class FileReader {
+
+	public static void main(String[] args) {
+		try {
+			java.io.FileReader fileReader = new java.io.FileReader("D:\\testout.txt");
+			int i;
+			while((i = fileReader.read())!=-1) {
+				System.out.print((char)i);
+			}
+			fileReader.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		
+	}
+
+}
