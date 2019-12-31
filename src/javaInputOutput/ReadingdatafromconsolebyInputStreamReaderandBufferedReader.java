0267a53e7088d2305d13d6250b1c47e45995a928
maainul <mainul080@yahoo.com>
2019-12-31 14:05:51 +0600

    bufferwriter class

---
+++ src/javaInputOutput/ReadingdatafromconsolebyInputStreamReaderandBufferedReader.java
@@ -0,0 +1,17 @@
+package javaInputOutput;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ReadingdatafromconsolebyInputStreamReaderandBufferedReader {
+
+	public static void main(String[] args) throws Exception{
+		InputStreamReader r = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(r);
+		System.out.println("Enter our name");
+		String nameString = br.readLine();
+		System.out.println("Welcome :"+nameString);
+	
+	}
+
+}
