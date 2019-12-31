a52f64095cb25b611196555e2f477545013a2ad5
maainul <mainul080@yahoo.com>
2019-12-31 12:55:09 +0600

    string reader

--- src/javaInputOutput/StringReader.java
+++ src/javaInputOutput/StringReader.java
@@ -4,7 +4,14 @@ public class StringReader {
 
 	public static void main(String[] args) {
 		try {
+			String string = "hello !it is not well";
+			java.io.StringReader name = new java.io.StringReader(string);
 			
+			int k = 0;
+			
+			while((k= name.read())!=-1) {
+				System.out.print((char)k);
+			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
