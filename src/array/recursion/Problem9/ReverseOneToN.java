8f3172d8d236640241782de9747bc36b66c78c37
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2020-01-14 11:31:21 +0600

    Create ReverseOneToN.java

---
+++ src/array/recursion/Problem9/ReverseOneToN.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+// Find the  reverse of integers from 1 through n.
+index :4                                                                                                                    
+index :3                                                                                                                    
+index :2                                                                                                                    
+index :1                                                                                                                    
+index :0
+
+*******************************************************************************/
+
+public class ReverseOneToN
+{
+	public static void main(String[] args) {
+       reverse(4);
+	   }
+	   
+	public static void reverse(int i){
+	    System.out.println("index :"+i);
+	    if(i == 0){
+	        return;
+	    }
+	    else{
+	         reverse(i-1);
+	    }
+	  
+	}
+	   
+}
+	
