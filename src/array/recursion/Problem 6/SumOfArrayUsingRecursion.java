4a56d3ba4ed1058a46c9688b33bc22c78bfc0c44
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2020-01-14 11:01:54 +0600

    Create SumOfArrayUsingRecursion.java

---
+++ src/array/recursion/Problem 6/SumOfArrayUsingRecursion.java
@@ -0,0 +1,35 @@
+/*
+input :  12,5,3,10
+
+Output:
+ index 0 :12                                                                                                                
+ index 1 :5                                                                                                                 
+ index 2 :3                                                                                                                 
+ index 3 :10                                                                                                                
+Sum of all digits 30    
+
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int sum = 0;
+		reverse(sum,0); 
+	}
+	
+	public static void reverse(int sum,int i){
+	    int array[] = {12,5,3,10};
+	    System.out.println(" index "+i+" :"+array[i]);
+	   
+	    sum = sum + array[i];
+	     
+	    if(i == array.length-1){
+	        System.out.println("Sum of all digits: "+sum);
+	        return;
+	    }else{
+	        reverse(sum,i+1);
+	    }
+	}
+}
+
