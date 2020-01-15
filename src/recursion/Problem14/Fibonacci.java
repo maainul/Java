298d59a5fb1493d781270b4a617afee858aea584
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2020-01-15 11:20:41 +0600

    Create Fibonacci.java

---
+++ src/recursion/Problem14/Fibonacci.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Write a recursive program to print fibonacci series. 1st and 2nd fibonacci numbers are 1, 1.
+
+Input:
+6
+Output:
+1 1 2 3 5 8 
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int x = 1;
+	    int y = 1;
+	    int z;
+	    System.out.print("1 1");
+	    for (int i = 2; i < 6;i++) {
+	        z = x +y;
+	        System.out.print(" "+ z);
+	        x = y;
+	        y = z;
+	    }
+	}
+}
