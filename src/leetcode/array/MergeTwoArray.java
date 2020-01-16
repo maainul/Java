1b115b69d82557bf61deceb4467787f8b93b616f
maainul <mainul080@yahoo.com>
2020-01-16 21:38:20 +0600

    update

--- src/leetcode/array/MergeTwoArray.java
+++ src/leetcode/array/MergeTwoArray.java
@@ -7,14 +7,13 @@ class Merge{
 	public static void merge(int[] nums1, int[] nums2, int m, int n) {
         int index=0;
         for(int i=m;i<m+n;i++) {
-        		nums1[i]=nums2[index++];
-        		System.out.println("Num 1 :"+nums1[i]);
-        		System.out.println("num2 :"+nums2[index]);
+        		nums1[i]=nums2[index++];        		
         }
         Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));
     }
 }
+
 
 public class MergeTwoArray {
 	public static void main(String[] args) {
@@ -27,6 +26,7 @@ public class MergeTwoArray {
 
 
 		Merge.merge(nums1, nums2,m,n);
+	
 		
 		
 	}
