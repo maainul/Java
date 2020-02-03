d331bd00eedaa762c64daad30cf0b0ffb06bbbc3
maainul <mainul080@yahoo.com>
2020-02-03 21:33:22 +0600

    gcd

---
+++ src/recursion/Problem20/GCDRecursion.java
@@ -0,0 +1,19 @@
+package recursion.Problem20;
+
+
+public class GCDRecursion {
+
+	public static void main(String[] args) {
+		System.out.println("GCD of Two Number\n");
+		int n1 = 68;
+		int n2 = 51;
+		System.out.println(gcdByEuclidsAlgorithm(n1, n2));
+	}
+
+	static int gcdByEuclidsAlgorithm(int n1, int n2) {
+	    if (n2 == 0) {
+	        return n1;
+	    }
+	    return gcdByEuclidsAlgorithm(n2, n1 % n2);
+	}
+}
