77a062b0ddd8d05c179b8ae6106e4d030f4ecc8a
maainul <mainul080@yahoo.com>
2020-02-03 14:59:59 +0600

    update code

--- src/leetcode/array/_53_MaximumSubArrayWithBruteForceMethod.java
+++ src/leetcode/array/_53_MaximumSubArrayWithBruteForceMethod.java
@@ -1,8 +1,17 @@
 package leetcode.array;
 
 /*
- * Java Program to Find the maximum subarray sum O(n^2)time 
- * (naive method)
+    53. Maximum Subarray
+
+	Given an integer array nums, 
+	find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+	Example:
+	
+	Input: [-2,1,-3,4,-1,2,1,-5,4],
+	Output: 6
+	Explanation: [4,-1,2,1] has the largest sum = 6.
+	
  */
 import java.util.Scanner;
  
@@ -10,6 +19,7 @@ public class _53_MaximumSubArrayWithBruteForceMethod
 {
     public static void main(String[] args) 
     {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter number of elements in array");
         int N = scan.nextInt();
@@ -20,19 +30,25 @@ public class _53_MaximumSubArrayWithBruteForceMethod
             arr[i] = scan.nextInt();
         System.out.println("Max sub array sum  = "+ max_sum(arr));
         scan.close();
+    	
     }
     public static int max_sum(int[] arr)
     {
         int N = arr.length, max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++)
         {
+        	
             int sum = 0;
+            
             for (int j = i; j < N; j++)
             {
                 sum += arr[j];
+               // System.out.println(j+" iteration sum = "+sum);
                 if (sum > max)
                     max = sum;
+                
             }
+            System.out.println(i+" iteration sum = "+sum);
         }
         return max;    
     }
