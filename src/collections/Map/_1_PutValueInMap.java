a96d07f4e5760cb97453165462f1782b28a4e2c2
maainul <mainul080@yahoo.com>
2020-02-04 18:58:19 +0600

    Map example

---
+++ src/collections/Map/_1_PutValueInMap.java
@@ -0,0 +1,20 @@
+package collections.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1_PutValueInMap {
+	public static void main(String args[]) {
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(100, "Anik");
+		map.put(101,"Manik");
+		map.put(102,"Rajib");
+		
+		// elements can traverse in any order
+		for(Map.Entry<Integer, String> m : map.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+	}
+
+}
---
+++ src/collections/Map/_3_ComparingByKeyInDescendingOrder.java
@@ -0,0 +1,29 @@
+package collections.Map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _3_ComparingByKeyInDescendingOrder {
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(100, "Anik");
+		map.put(101,"Manik");
+		map.put(102,"Rajib");
+		
+		//Returns a Set view of the mappings contained in this map  
+		map.entrySet()
+		
+		//Returns a sequential Stream with this collection as its source  
+		.stream()
+		
+		 //Sorted according to the provided Comparator  
+		.sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+		
+		//Performs an action for each element of this stream  
+		.forEach(System.out::println);
+		
+
+	}
+
+}
