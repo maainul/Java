44588b52c593d36ea5cc2ac275509f614b6f8c55
maainul <mainul080@yahoo.com>
2020-02-04 20:39:06 +0600

    compare by value

--- src/collections/Map/_4_MapExampleComparingByValue.java
+++ src/collections/Map/_4_MapExampleComparingByValue.java
@@ -1,22 +1,22 @@
 package collections.Map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;  
 
-public class _4_MapExampleComparingByValue {
+public class _4_MapExampleComparingByValue{  
 	
-	public static void main(String args) {
-		Map<Integer, String> map = new HashMap<Integer, String>();
-		map.put(100, "Onik");
-		map.put(101,"Oanik");
-		map.put(102,"Kajib");
+	public static void main(String args[]){  
+		Map<Integer,String> map=new HashMap<Integer,String>();          
+		map.put(100,"Amit");    
+		map.put(101,"Vijay");    
+		map.put(102,"Rahul");    
 		//Returns a Set view of the mappings contained in this map    
-	    map.entrySet()  
-	    //Returns a sequential Stream with this collection as its source  
-	    .stream()  
-	    //Sorted according to the provided Comparator  
-	    .sorted(Map.Entry.comparingByValue())  
-	    //Performs an action for each element of this stream  
-	    .forEach(System.out::println);  
-	}
-}
+		map.entrySet()  
+		//Returns a sequential Stream with this collection as its source  
+		.stream()  
+		//Sorted according to the provided Comparator  
+		.sorted(Map.Entry.comparingByValue())  
+		//Performs an action for each element of this stream  
+		.forEach(System.out::println);  
+ }  
+	
+}  
