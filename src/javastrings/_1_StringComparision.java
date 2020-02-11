df3a9f4bbad807528b523f64e553bcdd8357d86a
maainul <mainul080@yahoo.com>
2020-02-11 20:50:28 +0600

    add comment in string comparision

--- src/javastrings/_1_StringComparision.java
+++ src/javastrings/_1_StringComparision.java
@@ -11,26 +11,26 @@ public class _1_StringComparision {
 	String s5 = "mainul";
 	String s6 ="MAINUL";	
 	// equals to
-	System.out.println(s1.equals(s2));
-	System.out.println(s2.equals(s3));
-	System.out.println(s3.equals(s4));
-	System.out.println(s1.equals(s5));
-	System.out.println(s1.equals(s6));
+	System.out.println(s1.equals(s2)); // true
+	System.out.println(s2.equals(s3)); // false
+	System.out.println(s3.equals(s4)); // false
+	System.out.println(s1.equals(s5)); // false
+	System.out.println(s1.equals(s6)); // false
 	
 	// equalstoignore
-	System.out.println(s1.equalsIgnoreCase(s6));
+	System.out.println(s1.equalsIgnoreCase(s6)); //true
 	
 	//  == method
-	System.out.println(s1 == s2);
-	System.out.println(s1 == s5 );
+	System.out.println(s1 == s2); //true
+	System.out.println(s1 == s5 ); // false
 	/*
 		* compare to method
 		* s1 == s2 :0 
 		* s1 > s2 :positive value 
 		* s1 < s2 :negative value
 	*/
-	System.out.println(s1.compareTo(s2));
-	System.out.println(s1.compareTo(s4));
+	System.out.println(s1.compareTo(s2)); // 0
+	System.out.println(s1.compareTo(s4)); // -20
 	
 	}
 
