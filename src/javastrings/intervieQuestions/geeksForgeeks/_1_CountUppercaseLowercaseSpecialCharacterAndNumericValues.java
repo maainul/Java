9d904bf6cae3a5040f1190d4a253a2aa8b8ea4b6
maainul <mainul080@yahoo.com>
2020-02-14 15:19:19 +0600

    1089 duplicate 0

---
+++ src/javastrings/intervieQuestions/geeksForgeeks/_1_CountUppercaseLowercaseSpecialCharacterAndNumericValues.java
@@ -0,0 +1,48 @@
+package javastrings.intervieQuestions.geeksForgeeks;
+/*
+ *  1. uppercase
+ *  2. Lowercase
+ *  3. Special Character
+ *  4. Numerical value
+ *  
+ *  Java For Dummies Quick Reference Java has two operators 
+ *  for performing logical And operations: 
+ *  	& and &&. 
+ *  Both combine two Boolean expressions and return true 
+ *  only if both expressions are true. ... 
+ *  If they're both true, the & operator returns true. 
+ *  If one is false or both are false, 
+ *  the & operator returns false.
+ *  
+ */
+public class _1_CountUppercaseLowercaseSpecialCharacterAndNumericValues {
+
+	public static void main(String[] args) {// TODO Auto-generated method stub
+		int upperCase = 0, lowerCase = 0,specialCharacter = 0,numericalValue = 0;
+		
+		String s1 = "La Ilaha Illalahu Mohammadur Rasulullah!@11278(%sm%).";
+		
+		for (int i = 0; i < s1.length(); i++) {
+			
+			char ch = s1.charAt(i);
+			
+			if(ch >='A' && ch <='Z') {
+				upperCase++;
+			}else if (ch >= 'a' && ch <='z') {
+				lowerCase++;
+			}else if (ch >='0'&& ch <='9') {
+				numericalValue++;
+			}else {
+				specialCharacter++;
+			}
+			
+		}
+		
+		System.out.println("Upper Case Character :"+upperCase);
+		System.out.println("Lower Case Character:"+lowerCase);
+		System.out.println("Numerical Value:"+numericalValue);
+		System.out.println("Special Character :"+specialCharacter);
+		
+	}
+
+}
