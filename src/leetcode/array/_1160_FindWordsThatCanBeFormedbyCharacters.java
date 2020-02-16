ab6824c30758174d9d42b27803948088104790be
maainul <mainul080@yahoo.com>
2020-02-16 17:23:41 +0600

    add new class

---
+++ src/leetcode/array/_1160_FindWordsThatCanBeFormedbyCharacters.java
@@ -0,0 +1,27 @@
+package leetcode.array;
+
+public class _1160_FindWordsThatCanBeFormedbyCharacters {
+
+	public static void main(String[] args) {
+		 String words[] = {"cat","bt","hat","tree"};
+		 String chars = "atach";
+		 System.out.println(countCharacters(words,chars));
+		 
+	}
+	
+	 public static int countCharacters(String[] words, String chars) {
+		 
+		 for (int i = 0; i < words.length; i++) {
+			 
+			 
+			 
+			//System.out.println(words[i]);
+			//if(words[i]. == chars) {
+				//System.out.println(words[i]);
+			//}
+		}
+		 return 0;
+	        
+	    }
+
+}
