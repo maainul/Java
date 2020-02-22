adca542425dd2a23a2be6e6a61094e20bcd3306a
maainul <mainul080@yahoo.com>
2020-02-22 22:49:11 +0600

    reverse string without extra space

--- src/intervieQuestions/String/_17_FindReverseOfTheString/_17_FindReverseOfTheString.java
+++ src/intervieQuestions/String/_17_FindReverseOfTheString/_17_FindReverseOfTheString.java
@@ -28,6 +28,29 @@ public class _17_FindReverseOfTheString {
 			reverseString = reverseString + string.charAt(i);
 		}
 		System.out.println(reverseString);
+		
+		// method 2 :first make char of arry stirng;
+		char[] charry = string.toCharArray();
+		for (int i = charry.length-1; i >= 0; i--) {
+			System.out.print(charry[i]);
+		}
+		
+		// method 3 :String builder
+		System.out.println("\nMethod 3 (String Builder)\n");
+		String string2 = "Dream Very big";
+		StringBuilder sb = new StringBuilder();
+		for (int i = string2.length()-1; i >= 0; i--) {
+			sb.append(string2.charAt(i));
+		}
+		System.out.println(sb.toString());
+		
+		
+		// method 4 :String builder
+		System.out.println("\nMethod 4 (String Builder Reverse)\n");
+		String string3 = "Dream Very big";
+		StringBuilder sb2 = new StringBuilder(string3);
+		
+		System.out.println(sb2.reverse().toString());
 	}
 
 }
