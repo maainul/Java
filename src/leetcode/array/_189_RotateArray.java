cc74ef55c05f8477f4d1b4c2fce951e6861b5920
maainul <mainul080@yahoo.com>
2020-02-27 23:48:55 +0600

    rotate array 189

--- src/leetcode/array/_189_RotateArray.java
+++ src/leetcode/array/_189_RotateArray.java
@@ -3,31 +3,29 @@ package leetcode.array;
 public class _189_RotateArray {
 	public static void main(String[] args) {
 		
-		int[] nums = new int[] { 1, 2, 3, 4, 5 };
+		int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7};
 		int k = 3; 
 		System.out.println("Original array: ");
 		PrintArray(nums);
 		
-		k = k% nums.length;
-        reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.length-1);
-	
+		rotate(nums,k);
+       
 		System.out.println("\n\nArray after right rotation: ");
 		PrintArray(nums);
 		
 	}
 
-	private static void reverse(int[] nums, int start, int end) {
-		while(start<end){
-            int temp= nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-		
-	}
+	 public static void rotate(int[] nums, int k) {
+	        int temp, previous;
+	        for(int i=0;i<k;i++){
+	            previous = nums[nums.length-1];
+	            for(int j = 0; j<nums.length;j++){
+	                temp = nums[j];
+	                nums[j] = previous;
+	                previous = temp;
+	            }
+	        }
+	    }
 
 	private static void PrintArray(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
