49f1f757549ed9476e6b1c235252c896dae90893
maainul <mainul080@yahoo.com>
2020-02-29 23:41:42 +0600

    reverse integer

--- src/leetcode/Math/_7_ReverseInteger.java
+++ src/leetcode/Math/_7_ReverseInteger.java
@@ -24,19 +24,17 @@ public class _7_ReverseInteger {
 
 	public static void main(String args[]) {
 		int x = 4236469;
-		
+
 		System.out.println("Input : " + x + " Output : " + reverse(x));
 	}
 
-
-
 	public static int reverse(int x) {
-        long rev = 0;
-        while (x!= 0){
-            rev = (rev*10)+(x%10);
-            x = x/10;
-        }
-  
-        return (rev >= Integer.MAX_VALUE || rev <=Integer.MIN_VALUE) ? 0 : (int)rev;
-    	}
+		long rev = 0;
+		while (x != 0) {
+			rev = (rev * 10) + (x % 10);
+			x = x / 10;
+		}
+
+		return (rev >= Integer.MAX_VALUE || rev <= Integer.MIN_VALUE) ? 0 : (int) rev;
+	}
 }
