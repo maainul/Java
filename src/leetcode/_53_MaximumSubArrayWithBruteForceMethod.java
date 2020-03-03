a6075123bf2a78aece466b7d7cfbe63e9de1fd11
maainul <mainul080@yahoo.com>
2020-03-03 21:25:18 +0600

    Update string

--- src/leetcode/_53_MaximumSubArrayWithBruteForceMethod.java
+++ src/leetcode/_53_MaximumSubArrayWithBruteForceMethod.java
@@ -20,36 +20,23 @@ public class _53_MaximumSubArrayWithBruteForceMethod
     public static void main(String[] args) 
     {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter number of elements in array");
-        int N = scan.nextInt();
-        int[] arr = new int[ N ];
-        /* Accept N elements */
-        System.out.println("Enter "+ N +" elements");
-        for (int i = 0; i < N; i++)
-            arr[i] = scan.nextInt();
-        System.out.println("Max sub array sum  = "+ max_sum(arr));
-        scan.close();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
     	
     }
-    public static int max_sum(int[] arr)
-    {
-        int N = arr.length, max = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++)
-        {
-        	
-            int sum = 0;
-            
-            for (int j = i; j < N; j++)
-            {
-                sum += arr[j];
-               // System.out.println(j+" iteration sum = "+sum);
-                if (sum > max)
-                    max = sum;
-                
+   
+    public int maxSubArray(int[] nums) {
+        int msf = Integer.MIN_VALUE;
+        int meh = 0;
+        for(int i = 0; i<nums.length;i++){
+            meh +=nums[i];
+            if(meh < nums[i]){
+                meh = nums[i];
             }
-            System.out.println(i+" iteration sum = "+sum);
+            if(msf < meh){
+                msf = meh;
+            }
         }
-        return max;    
+        return msf;
     }
 }
