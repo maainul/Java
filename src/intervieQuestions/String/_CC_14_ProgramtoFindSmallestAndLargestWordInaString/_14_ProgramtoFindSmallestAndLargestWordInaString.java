9f9a9beaa6810943f0f1d315c2acd2da1ec57752
maainul <mainul080@yahoo.com>
2020-03-08 21:32:02 +0600

    find largest and smallest word

---
+++ src/intervieQuestions/String/_CC_14_ProgramtoFindSmallestAndLargestWordInaString/_14_ProgramtoFindSmallestAndLargestWordInaString.java
@@ -0,0 +1,36 @@
+package intervieQuestions.String._CC_14_ProgramtoFindSmallestAndLargestWordInaString;
+
+//Java program to Find the Largest and Smallest Word.
+
+public class _14_ProgramtoFindSmallestAndLargestWordInaString {
+	public static void main(String args[]) {
+		String str = "This is mainul Hasan";
+		largestAndSmallWord(str);
+	}
+	
+	public static void largestAndSmallWord(String s) {
+		// 1. split word
+		String[] arr = s.split(" ");
+		// 2. declear value
+		int maxLength,minLength;
+		String smallest,largest;
+		smallest = largest = "";
+		// assign value max and min
+		maxLength = Integer.MIN_VALUE;
+		minLength = Integer.MAX_VALUE;
+		// loop through all word
+		for(int i = 0; i<arr.length;i++) {
+			if(arr[i].length() < minLength) {// minLength = 2147483648
+				minLength = arr[i].length();
+				smallest = arr[i];
+			}
+			if(arr[i].length() > maxLength) { // maxLength = -2147483648
+				maxLength = arr[i].length();
+				largest = arr[i];
+			}
+		}
+		
+		System.out.println("Largest value :"+largest+"\nSmallest Word : "+smallest);
+	}
+
+}
