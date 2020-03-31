2e9b1f56bcbf01a926a81c3e210a85b6f2aca80e
maainul <mainul080@yahoo.com>
2020-03-31 07:15:59 +0600

    update readme

---
+++ src/intervieQuestions/String/CC_45_CountofCharacterPairsAtSameDistanceAsinEnglishAlphabets/CC_45_CountofCharacterPairsAtSameDistanceAsinEnglishAlphabets.java
@@ -0,0 +1,18 @@
+package intervieQuestions.String.CC_45_CountofCharacterPairsAtSameDistanceAsinEnglishAlphabets;
+
+public class CC_45_CountofCharacterPairsAtSameDistanceAsinEnglishAlphabets {
+	public static void main(String[] args) {
+		String str = "oservation";
+		int result = 0;
+		int n = str.length();
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j < n; j++)
+
+				// Increment count if characters
+				// are at same distance
+				if (Math.abs(str.charAt(i) - str.charAt(j)) == Math.abs(i - j))
+					result++;
+
+		System.out.println(result);
+	}
+}
