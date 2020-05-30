18c02868604c97d34f0804767b82f6da2532cc74
maainul <mainul080@yahoo.com>
2020-05-30 15:27:15 +0600

    find length

---
+++ src/linkedList/_7_FindLength.java
@@ -0,0 +1,58 @@
+package linkedList;
+
+public class _7_FindLength {
+    ListNode head;
+    public class ListNode {
+        int data;
+        ListNode next;
+        ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private void insertAtLast(int data){
+        ListNode newNode = new ListNode(data);
+        ListNode current = head;
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    private  void display(){
+        ListNode current = head;
+        while (current != null){
+            System.out.print(current.data+"->");
+            current = current.next;
+        }
+    }
+    public int length(){
+        ListNode c = head;
+        int len = 0;
+        while (c != null){
+            c = c.next;
+            len++;
+        }
+        return  len;
+    }
+
+    public static void main(String[] args) {
+        _7_FindLength obj = new _7_FindLength();
+        obj.insertAtLast(1);
+        obj.insertAtLast(2);
+        obj.insertAtLast(3);
+        obj.insertAtLast(4);
+        obj.insertAtLast(5);
+        obj.insertAtLast(6);
+
+        obj.display();
+        System.out.println();
+        System.out.println(obj.length());
+
+    }
+}
+
