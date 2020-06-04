f363952aa3dbc7cf80d3859fc8fae844e527345d
maainul <mainul080@yahoo.com>
2020-06-04 18:40:20 +0600

    loop in linked list

--- src/linkedList/_40_DeleteNode.java
+++ src/linkedList/_40_DeleteNode.java
@@ -1,5 +1,26 @@
 package linkedList;
+/*
+Given a Singly Linked List, write a function to delete a given node. Your function must follow following constraints:
+1) It must accept a pointer to the start node as the first parameter and node to be deleted as the second parameter i.e., 
+a pointer to head node is not global.
+2) It should not return a pointer to the head node.
+3) It should not accept pointer to pointer to the head node.
 
+You may assume that the Linked List never becomes empty.
+
+Let the function name be deleteNode(). In a straightforward implementation, 
+the function needs to modify the head pointer when the node to be deleted is the first node. 
+As discussed in previous post, when a function modifies the head pointer, 
+the function must use one of the given approaches, we can’t use any of those approaches here
+
+Given Linked List: 12 15 10 11 5 6 2 3
+
+Deleting node 10:
+Modified Linked List: 12 15 11 5 6 2 3
+
+Deleting first node
+Modified Linked List: 15 11 5 6 2 3
+*/
 public class _40_DeleteNode {
 
     ListNode head;
