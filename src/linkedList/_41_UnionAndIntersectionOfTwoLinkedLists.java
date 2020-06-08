031cfeebe84f8a62bcc24e44419f8bd9c4e84a87
maainul <mainul080@yahoo.com>
2020-06-07 22:32:34 -0700

    add problem statement

--- src/linkedList/_41_UnionAndIntersectionOfTwoLinkedLists.java
+++ src/linkedList/_41_UnionAndIntersectionOfTwoLinkedLists.java
@@ -1,5 +1,17 @@
 package linkedList;
+/*
+Union and Intersection of two Linked Lists
+Given two Linked Lists, create union and intersection lists that contain union and intersection of
+the elements present in the given lists. Order of elments in output lists doesn’t matter.
+Example:
 
+Input:
+   List1: 10->15->4->20
+   lsit2:  8->4->2->10
+Output:
+   Intersection List: 4->10
+   Union List: 2->8->20->4->15->10
+ */
 public class _41_UnionAndIntersectionOfTwoLinkedLists {
 
     ListNode head;
