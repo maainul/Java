9c08bd241e85aa4d08d65871d9ef86328c0d446d
maainul <mainul080@yahoo.com>
2020-06-07 22:39:25 -0700

    add problem statement details

--- src/linkedList/_41_UnionAndIntersectionOfTwoLinkedLists.java
+++ src/linkedList/_41_UnionAndIntersectionOfTwoLinkedLists.java
@@ -8,9 +8,11 @@ Example:
 Input:
    List1: 10->15->4->20
    lsit2:  8->4->2->10
+
 Output:
    Intersection List: 4->10
    Union List: 2->8->20->4->15->10
+   
  */
 public class _41_UnionAndIntersectionOfTwoLinkedLists {
 
