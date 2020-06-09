96876d8ac4856fbda782680a98291e4774e9903a
maainul <mainul080@yahoo.com>
2020-06-09 07:05:51 +0600

    reverse linked list

--- src/linkedList/_46_SortLinkedList0s1s2s.java
+++ src/linkedList/_46_SortLinkedList0s1s2s.java
@@ -1,57 +1,113 @@
 package linkedList;
 
-public class _46_SortLinkedList0s1s2s {
+import java.util.zip.ZipEntry;
 
-    ListNode head;
-    public class ListNode {
-        int data;
-        ListNode next;
-        ListNode(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
+/*
+Sort a linked list of 0s, 1s and 2s by changing links
+Given a linked list of 0s, 1s and 2s, sort it.
 
-    private void insertAtLast(int data){
-        ListNode newNode = new ListNode(data);
-        ListNode current = head;
-        if(head == null){
-            head = newNode;
-        }
-        else{
-            while (current.next != null){
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
+Examples:
 
-    private  void display(){
-        ListNode current = head;
-        while (current != null){
-            System.out.print(current.data+"->");
-            current = current.next;
-        }
-    }
+Input  : 2->1->2->1->1->2->0->1->0
+Output : 0->0->1->1->1->1->2->2->2
 
-    public static void main(String[] args) {
-        _46_SortLinkedList0s1s2s obj = new _46_SortLinkedList0s1s2s();
+Input : 2->1->0
+Output : 0->1->2
 
-        obj.insertAtLast(1);
-        obj.insertAtLast(2);
+ */
 
-        obj.insertAtLast(2);
-        obj.insertAtLast(0);
-
-        obj.insertAtLast(0);
-        obj.insertAtLast(1);
-
-        obj.insertAtLast(2);
-        obj.insertAtLast(0);
-
-        obj.display();
-        System.out.println();
-
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data=data;
     }
 }
+public class _46_SortLinkedList0s1s2s {
+
+
+        // Sort a linked list of 0s, 1s and 2s
+        // by changing pointers.
+        public static Node sortList(Node head)
+        {
+            if(head==null || head.next==null)
+            {
+                return head;
+            }
+            // Create three dummy nodes to point to
+            // beginning of three linked lists. These
+            // dummy nodes are created to avoid many
+            // null checks.
+            Node zeroD = new Node(0);
+            Node oneD = new Node(0);
+            Node twoD = new Node(0);
+
+            // Initialize current pointers for three
+            // lists and whole list.
+            Node zero = zeroD, one = oneD, two = twoD;
+            // Traverse list
+            Node curr = head;
+            while (curr!=null)
+            {
+                if (curr.data == 0)
+                {
+                    zero.next = curr;
+                    zero = zero.next;
+                    curr = curr.next;
+                }
+                else if (curr.data == 1)
+                {
+                    one.next = curr;
+                    one = one.next;
+                    curr = curr.next;
+                }
+                else
+                {
+                    two.next = curr;
+                    two = two.next;
+                    curr = curr.next;
+                }
+            }
+            // Attach three lists
+            zero.next = (oneD.next!=null) ? (oneD.next) : (twoD.next);
+            one.next = twoD.next;
+            two.next = null;
+            // Updated head
+            head = zeroD.next;
+            return head;
+        }
+
+        // function to create and return a node
+        public static Node newNode(int data)
+        {
+            // allocating space
+            Node newNode = new Node(data);
+            newNode.next = null;
+            return newNode;
+        }
+
+        /* Function to print linked list */
+        public static void printList(Node node)
+        {
+            while (node != null)
+            {
+                System.out.print(node.data+" ");
+                node = node.next;
+            }
+        }
+
+        public static void main(String args[]) {
+            Node head = new Node(1);
+            head.next = new Node(2);
+            head.next.next = new Node(0);
+            head.next.next.next = new Node(1);
+            System.out.println("Linked List Before Sorting");
+            printList(head);
+            head = sortList(head);
+            System.out.println("\nLinked List After Sorting");
+            printList(head);
+        }
+    }
 
