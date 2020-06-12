77bc933e1b83c98001bfffae71165e52b766d36e
maainul <mainul080@yahoo.com>
2020-06-12 15:28:14 +0600

    search recursive

--- src/linkedList/_8_SearchElement.java
+++ src/linkedList/_8_SearchElement.java
@@ -47,6 +47,36 @@ public class _8_SearchElement {
         }
     }
 
+    private boolean searchElement(int searchVal){
+        ListNode current = head;
+        while (current != null){
+            if (current.data == searchVal){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    // Recursive solution
+    private boolean searchRecursive(ListNode head,int x){
+
+        // Base case
+        if (head == null)
+            return false;
+
+        // If key is present in current node,
+        // return true
+        if (head.data == x)
+            return true;
+
+        // Recur for remaining list
+        return searchRecursive(head.next, x);
+    }
+
+
+
 
     public static void main(String[] args) {
         _8_SearchElement obj = new _8_SearchElement();
@@ -62,6 +92,20 @@ public class _8_SearchElement {
         int v = 4;
         System.out.println("\nSearch value :"+v);
         obj.search(v);
+
+        if(obj.searchElement(v)){
+            System.out.println("Search key found.");
+        }else{
+            System.out.println("Search Key Not found.");
+        }
+
+        if (obj.searchRecursive(obj.head, 4)){
+            System.out.println("YES");
+        }else{
+            System.out.println("False");
+        }
+
+
 
     }
 }
