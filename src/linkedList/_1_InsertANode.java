da61182a37df4a29c75e92e6e1001a657ff01373
maainul <mainul080@yahoo.com>
2020-06-13 09:29:53 +0600

    create greater than two list

--- src/linkedList/_1_InsertANode.java
+++ src/linkedList/_1_InsertANode.java
@@ -1,4 +1,5 @@
 package linkedList;
+
 /* insert a node in a given position
 Original Linked list
 1->2->3->4->
@@ -7,67 +8,69 @@ After insert Value
 1->2->10->3->4->
  */
 public class _1_InsertANode {
-   ListNode head;
-    public class ListNode {
-        int data;
-        ListNode next;
-        ListNode(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    private void insertAtLast(int data){
-        ListNode newNode = new ListNode(data);
-        ListNode current = head;
-        if(head == null){
-            head = newNode;
-        }
-        else{
-            while (current.next != null){
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-    private  void display(){
-        ListNode current = head;
-        while (current != null){
-            System.out.print(current.data+"->");
-            current = current.next;
-        }
-    }
+	ListNode head;
 
-    private void insertNode(ListNode head, int k,int data) {
-        ListNode newNode = new ListNode(data);
+	public class ListNode {
+		int data;
+		ListNode next;
 
-        ListNode currentListNode = head;
-        ListNode previousListNode = null;
-        int count = 0;
-        while (count < k && currentListNode != null) {
-            previousListNode = currentListNode;
-            currentListNode = currentListNode.next;
-            count++;
-        }
-        previousListNode.next = newNode;
-        newNode.next = currentListNode;
-    }
+		ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
 
-    public static void main(String[] args) {
-        _1_InsertANode obj = new _1_InsertANode();
-        obj.insertAtLast(1);
-        obj.insertAtLast(2);
-        obj.insertAtLast(3);
-        obj.insertAtLast(4);
+	private void insertAtLast(int data) {
+		ListNode newNode = new ListNode(data);
+		ListNode current = head;
+		if (head == null) {
+			head = newNode;
+		} else {
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+	}
 
-        obj.display();
-        System.out.println();
+	private void display() {
+		ListNode current = head;
+		while (current != null) {
+			System.out.print(current.data + "->");
+			current = current.next;
+		}
+	}
 
-        obj.insertNode(obj.head, 2, 10);
+	private void insertNode(ListNode head, int k, int data) {
+		ListNode newNode = new ListNode(data);
 
-        System.out.println("After insert Value ");
-        obj.display();
+		ListNode currentListNode = head;
+		ListNode previousListNode = null;
+		int count = 0;
+		while (count < k && currentListNode != null) {
+			previousListNode = currentListNode;
+			currentListNode = currentListNode.next;
+			count++;
+		}
+		previousListNode.next = newNode;
+		newNode.next = currentListNode;
+	}
 
+	public static void main(String[] args) {
+		_1_InsertANode obj = new _1_InsertANode();
+		obj.insertAtLast(1);
+		obj.insertAtLast(2);
+		obj.insertAtLast(3);
+		obj.insertAtLast(4);
 
-    }
+		obj.display();
+		System.out.println();
+
+		obj.insertNode(obj.head, 2, 10);
+
+		System.out.println("After insert Value ");
+		obj.display();
+
+	}
 
 }
