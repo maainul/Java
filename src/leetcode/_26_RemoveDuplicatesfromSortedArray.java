c4ebe1549241e85874c45a887fad2c9c419add2e
maainul <mainul080@yahoo.com>
2020-06-13 13:56:18 +0600

    remove duplicate

--- src/leetcode/_26_RemoveDuplicatesfromSortedArray.java
+++ src/leetcode/_26_RemoveDuplicatesfromSortedArray.java
@@ -25,25 +25,25 @@ package leetcode;
 
 public class _26_RemoveDuplicatesfromSortedArray {
 
+	public static int removeDuplicates(int[] nums) {
+		if(nums == null || nums.length == 0){
+			return 0;
+		}
+		int count = 1;
+		for(int i = 1;i<nums.length;i++){
+			if(nums[i-1] != nums[i]){
+				nums[count] = nums[i];
+				count++;
+			}
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
 		int[] nums = {1,1,2,2,3};
 		System.out.println((removeDuplicates(nums)));
 		
 		
 	}
-	
-	public static int removeDuplicates(int[] nums) {
-	    if(nums == null || nums.length == 0){
-	        return 0;
-	    }
-	    int count = 1;
-	    for(int i = 1;i<nums.length;i++){
-	        if(nums[i-1] != nums[i]){
-	            nums[count] = nums[i];
-	            count++;
-	        }    
-	    }
-	        return count;
-	    }
-
 }
+
