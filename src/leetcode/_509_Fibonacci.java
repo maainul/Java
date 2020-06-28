58b0d8f7325755819ed0d1db5ebf5cba9b11dd62
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2020-06-28 14:10:02 +0600

    Update _509_Fibonacci.java

--- src/leetcode/_509_Fibonacci.java
+++ src/leetcode/_509_Fibonacci.java
@@ -6,9 +6,11 @@ public class _509_Fibonacci {
 		// 1(1) 1(2) 2(3) 3(4) 5(5) 8(6) 13(7) 21(8)
 		int n = 4;
 		System.out.println(fib(n)); 
-        for (int i = 1; i <= n; i++) {
+                for (int i = 1; i <= n; i++) {
 			System.out.print(fib(i)+" ");
 		}
+		System.out.println(); 
+		System.out.println(fib2(n)); 
 	}
 	
 	public static int fib(int N) {
@@ -22,6 +24,17 @@ public class _509_Fibonacci {
 	    } 
 	    return b; 
 	 } 
+	
+	public int fib2(int N) {
+		if(N == 0) return 0;
+		int[] arr = new int[N+1];
+		arr[0] = 0;
+		arr[1] = 1;
+		for(int i =2; i <=N; i++){
+		    arr[i] = arr[i-1] + arr[i-2];
+		}
+		return arr[N];
+    }
 }
 
 
