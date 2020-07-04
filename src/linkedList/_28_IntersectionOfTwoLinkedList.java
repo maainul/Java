d784da416e8051d96768eae2c930313458253ffa
maainul <mainul080@yahoo.com>
2020-07-04 22:46:10 +0600

    adjacency matrix of graph code

--- src/linkedList/_28_IntersectionOfTwoLinkedList.java
+++ src/linkedList/_28_IntersectionOfTwoLinkedList.java
@@ -2,10 +2,19 @@
 package linkedList;
 /*
 Intersection of two Sorted Linked Lists
+
 Given two lists sorted in increasing order,
+
 create and return a new list representing the intersection of the two lists.
+
 The new list should be made with its own memory — the original lists should not be changed.
-For example, let the first linked list be 1->2->3->4->6 and second linked list be 2->4->6->8,
+
+For example, let the 
+
+first linked list be 1->2->3->4->6 
+
+second linked list be 2->4->6->8,
+
 then your function should create and return a third list as 2->4->6.
 
     2->4->6->8->
