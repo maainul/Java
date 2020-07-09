6cd3a562950f8707f68d479f2f645ced8c80f4d8
maainul <mainul080@yahoo.com>
2020-07-09 19:56:13 +0600

    update code

--- src/linkedList/_56_MakeaLoopAtkthPosition.java
+++ src/linkedList/_56_MakeaLoopAtkthPosition.java
@@ -6,83 +6,82 @@ package linkedList;
 
 public class _56_MakeaLoopAtkthPosition {
 
-    ListNode head;
+	ListNode head;
 
-    public static class ListNode {
-        int data;
-        ListNode next;
+	public static class ListNode {
+		int data;
+		ListNode next;
 
-        ListNode(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+		ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
 
-    private void insertAtLast(int data) {
-        ListNode newNode = new ListNode(data);
-        ListNode current = head;
-        if (head == null) {
-            head = newNode;
-        } else {
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-   
-    private static void printLoop(ListNode head, int total_nodes) {
-        ListNode cur = head;
-        int count = 0;
-        while (count < total_nodes){
-            System.out.print(cur.data+" ");
-            count++;
-            cur = cur.next;
-        }
-    }
+	private void insertAtLast(int data) {
+		ListNode newNode = new ListNode(data);
+		ListNode current = head;
+		if (head == null) {
+			head = newNode;
+		} else {
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+	}
 
-    private ListNode makeLoop(ListNode head, int i) {
-        ListNode temp = head;
-        int count = 1;
-        while (count < i ){
-            temp = temp.next;
-            count++;
-        }
-        ListNode joint_node = temp;
-        while (temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = joint_node;
-        return head;
-    }
+	private static void printLoop(ListNode head, int total_nodes) {
+		ListNode cur = head;
+		int count = 0;
+		while (count < total_nodes) {
+			System.out.print(cur.data + " ");
+			count++;
+			cur = cur.next;
+		}
+	}
 
-    private static int countNodes(ListNode head) {
-        int count = 0;
-        while (head != null) {
-            head = head.next;
-            count++;
-        }
-        return count;
-    }
+	private ListNode makeLoop(ListNode head, int i) {
+		ListNode temp = head;
+		int count = 1;
+		while (count < i) {
+			temp = temp.next;
+			count++;
+		}
+		ListNode joint_node = temp;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = joint_node;
+		return head;
+	}
 
-    public static void main(String[] args) {
-        _56_MakeaLoopAtkthPosition obj1 = new _56_MakeaLoopAtkthPosition();
-        obj1.insertAtLast(7);
-        obj1.insertAtLast(8);
-        obj1.insertAtLast(3);
-        obj1.insertAtLast(5);
-        obj1.insertAtLast(17);
-        
-        int total_nodes = countNodes(obj1.head);
-        System.out.println("Original Linked list");
-        printLoop(obj1.head,total_nodes);
+	private static int countNodes(ListNode head) {
+		int count = 0;
+		while (head != null) {
+			head = head.next;
+			count++;
+		}
+		return count;
+	}
 
-        System.out.println("\nAfter making loop");
-        obj1.makeLoop(obj1.head, 4);
+	public static void main(String[] args) {
+		_56_MakeaLoopAtkthPosition obj1 = new _56_MakeaLoopAtkthPosition();
+		obj1.insertAtLast(7);
+		obj1.insertAtLast(8);
+		obj1.insertAtLast(3);
+		obj1.insertAtLast(5);
+		obj1.insertAtLast(17);
 
-        printLoop(obj1.head,total_nodes);
+		int total_nodes = countNodes(obj1.head);
+		System.out.println("Original Linked list");
+		printLoop(obj1.head, total_nodes);
 
-    }
+		System.out.println("\nAfter making loop");
+		obj1.makeLoop(obj1.head, 4);
 
+		printLoop(obj1.head, total_nodes);
+
+	}
 
 }
