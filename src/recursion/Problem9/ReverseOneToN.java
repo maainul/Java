1d521d20c84f1ae422480e0b0cf87f4aeb51cb2f
maainul <mainul080@yahoo.com>
2020-07-14 00:05:10 +0600

    update one to n

--- src/recursion/Problem9/ReverseOneToN.java
+++ src/recursion/Problem9/ReverseOneToN.java
@@ -9,23 +9,30 @@ index :0
 *******************************************************************************/
 package recursion.Problem9;
 
-
-public class ReverseOneToN
-{
+public class ReverseOneToN {
 	public static void main(String[] args) {
-       reverse(4);
-	   }
-	   
-	public static void reverse(int i){
-	    System.out.println("index :"+i);
-	    if(i == 0){
-	        return;
-	    }
-	    else{
-	         reverse(i-1);
-	    }
-	  
+		reverse(4);
+		int n = 5;
+		reversearray(n);
 	}
-	   
+
+	private static void reversearray(int n) {
+		System.out.println(n);
+		if (n == 0) {
+			return;
+		}
+		reversearray(n - 1);
+
+	}
+
+	public static void reverse(int i) {
+		System.out.println("index :" + i);
+		if (i == 0) {
+			return;
+		} else {
+			reverse(i - 1);
+		}
+
+	}
+
 }
-	
