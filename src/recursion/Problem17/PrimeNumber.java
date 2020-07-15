154687d55ab19738708986f6125080c74885e21d
maainul <mainul080@yahoo.com>
2020-07-15 16:17:07 +0600

    prime number

--- src/recursion/Problem17/PrimeNumber.java
+++ src/recursion/Problem17/PrimeNumber.java
@@ -1,43 +1,37 @@
-//https://www.geeksforgeeks.org/prime-numbers/
-
+// https://www.programiz.com/java-programming/examples/prime-number
 package recursion.Problem17;
-//A school method based Java program to 
-//check if a number is prime 
 
 import java.util.Scanner;
 
-class PrimeNumber { 
-	
-	// function check whether a number 
-	// is prime or not 
-	static boolean isPrime(int n) 
-	{ 
-		// Corner case 
-		if (n <= 1) 
-			return false; 
-	
-		// Check from 2 to n-1 
-		for (int i = 2; i < n; i++) 
-			if (n % i == 0) 
-				return false; 
-	
-		return true; 
-	} 
-	
-	/* Driver program */
-	public static void main(String[] args) 
-	{ 
+class PrimeNumber {
+	public static void main(String[] args) {
 		System.out.println("Enter a number :");
-		
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		s.close();
-		
-	    if (isPrime(n))  
-	    	System.out.println("Yes Prime Number"); 
-	    else 
-	        System.out.println("Not Prime Number"); 
-	} 
-} 
-	
-//This code is contributed by Arnav Kr. Mandal	 
+		primeNumber(n);
+	}
+
+	private static void primeNumber(int n) {
+		boolean flage = false;
+		for (int i = 2; i < n / 2; i++) {
+			if (n % i == 0) {
+				flage = true;
+				break;
+			}
+		}
+		if (flage) {
+			System.out.println("Not a prime number");
+		} else {
+			System.out.println("Prime Number");
+		}
+
+	}
+}
+
+// Output
+/*
+Enter a number :
+7
+Prime Number
+*/
--- src/recursion/Problem18/Problem18.java
+++ src/recursion/Problem18/Problem18.java
@@ -25,41 +25,38 @@ Some interesting fact about Prime numbers
  
  ***********************************************************/
 
-
-
-
-
 package recursion.Problem18;
 
-import java.util.Scanner;
-
-public class PrimeNumber {
-
+public class Problem18 {
 	public static void main(String[] args) {
-		System.out.println("Enter a number :");
-		
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		s.close();
-	    if (isPrime(n, 2))  
-	    	System.out.println("Yes Prime Number"); 
-	    else 
-	        System.out.println("Not Prime Number"); 
-	}
-	
-	static boolean isPrime(int n, int i) 
-    { 
-        // Base cases 
-        if (n <= 2) 
-            return (n == 2) ? true : false; 
-        if (n % i == 0) 
-            return false; 
-        if (i * i > n) 
-            return true; 
-       
-        // Check for next divisor 
-        return isPrime(n, i + 1); 
-    } 
-      
+		int n = 11;
+		int prime = primeNumber(n, n / 2);
+		if (prime == 1) {
+			System.out.println("A prime number");
+		} else {
+			System.out.println("Not a Prime Number");
+		}
 
+	}
+
+	private static int primeNumber(int n, int i) {
+		if (i == 1) {
+			return 1;
+		} else {
+			if (n % i == 0) {
+				return 0;
+			} else {
+				return primeNumber(n, i - 1);
+			}
+		}
+	}
 }
+
+//Output
+/*******
+ * 
+ * Enter a number : 7
+ * 
+ * Prime Number
+ * 
+ ******/
