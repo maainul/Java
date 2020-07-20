ad1c01be8bf2d34bee74ed65539bb593a30ddf0f
maainul <mainul080@yahoo.com>
2020-07-20 23:05:06 +0600

    change package name

---
+++ src/dataStructureAndAlgorithm/_1_array/Main.java
@@ -0,0 +1,56 @@
+package dataStructureAndAlgorithm._1_array;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Array practice = new Array();
+		// insert value to the array...
+		practice.insertArray(54);
+		practice.insertArray(43);
+		practice.insertArray(98);
+		practice.insertArray(98);
+		practice.insertArray(101);
+		System.out.print("Original array is: ");
+		practice.show();
+		// insert value using positon...
+		//practice.insertAt(2, 10);
+	  //  System.out.print("After inserting elements value is :");
+	//	practice.show();
+		// delete using value
+		//practice.delete(71);
+	   // practice.delete(12);
+		//System.out.println("After delete value elements are: ");
+		//practice.show();
+		//delete by index number
+	//	practice.deleteAt(1);
+	//	practice.show();
+	//	practice.deleteAt(0);
+	//	practice.show();
+		//search by value...
+	practice.search(54);
+		practice.show();
+		System.out.println();
+		practice.removeDuplicate();
+		//practice.show();
+		
+				
+
+	}
+
+}
+
+/*
+Output:
+	Original array is: 89 71 12 7 
+	After inserting elements value is :89 71 10 7 
+	Delete successfully
+	Elemnet is not found...
+	After delete value elements are: 
+	89 10 7 0 
+	Delete successfully
+	89 7 0 0 
+	Delete successfully
+	7 0 0 0 
+	Element 7 is present in the index number: 0
+	7 0 0 0 
+*/
---
+++ src/dataStructureAndAlgorithm/_1_array/MergeWithOnlyValue.java
@@ -0,0 +1,43 @@
+package dataStructureAndAlgorithm._1_array;
+
+import java.util.Arrays;
+
+
+class MergeArray{
+	public static void merge(int[] nums1, int[] nums2,int m, int n) {
+		
+		m = nums1.length;
+		n = nums2.length;
+		
+		System.out.println(m);
+		System.out.println(n);
+		
+		int[] result = new int [m +n];
+		
+		System.arraycopy(nums1, 0, result, 0, m);
+		System.arraycopy(nums2, 0, result, m, n);
+		
+		System.out.println(Arrays.toString(result));
+		
+		Arrays.sort(result);
+		System.out.println(Arrays.toString(result));
+	}
+	
+}
+
+public class MergeWithOnlyValue {
+	public static void main(String[] args) {
+		
+		int nums1[] = {2,5,7,8};
+		int nums2[] = {2,4,6};
+		
+		int m = nums1.length;
+		int n = nums2.length;
+
+
+		MergeArray.merge(nums1, nums2,m,n);
+		
+		
+	}
+
+}
