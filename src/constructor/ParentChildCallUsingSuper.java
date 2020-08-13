933d31d88d4db695479f9088ee22d8bdbc14963a
maainul <mainul080@yahoo.com>
2020-08-13 07:32:47 +0600

    constructor

---
+++ src/constructor/ParentChildCallUsingSuper.java
@@ -0,0 +1,19 @@
+package constructor;
+
+class Parent{
+	public Parent() {
+		// TODO Auto-generated constructor stub
+	}
+}
+class Child extends Parent{
+	
+}
+public class ParentChildCallUsingSuper {
+
+
+	public static void main(String[] args) {
+		Child obj = new Child();
+
+	}
+
+}
