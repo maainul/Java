4d5942a83ceb23a63495295a49448f399c749d4d
maainul <mainul080@yahoo.com>
2020-08-22 11:57:04 +0600

    update

---
+++ src/javastrings/FindTheFirstNonRepeatingCharacterInAString.java
@@ -0,0 +1,91 @@
+package javastrings;
+
+import java.util.Scanner;
+
+/*
+Find the first non-repeating character in a string | FACE Prep
+
+Published on 11 Mar 2020
+
+Program to find the first non-repeating character in a string is discussed here. 
+
+Given a string, the task is to find the first non-repeating character in the string.
+
+
+
+Input and Output format:
+
+The first line of the input consists of a string.
+
+
+
+Sample Input 1:
+
+teeterson
+
+
+Sample Output 1:
+
+r
+
+
+Sample Input 2:
+
+charactercharacter
+
+
+
+Sample Output 2:
+
+All characters are repetitive
+
+
+Algorithm to find the first non-repeating character in a string
+
+
+1. Input the string from the user.
+
+2. Start traversing the string using two loops.
+
+3. Use the first loop to scan the characters of the string one by one.
+
+4. Use the second loop to find if the current character is occurring in the latter part if the string or not.
+
+5. If it is not occurring, print that character.
+
+Else, continue traversing.
+
+*/
+public class FindTheFirstNonRepeatingCharacterInAString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String string = sc.nextLine();
+		sc.close();
+		int result = firstRepeatingChar(string);
+
+		System.out.println(result == -1 ? "All are repeatative" : string.charAt(result));
+	}
+
+	private static int firstRepeatingChar(String string) {
+		int[] freq = new int[26];
+		int index = -1;
+		for (int i = 0; i < string.length(); i++) {
+			freq[string.charAt(i) - 'a']++;
+		}
+
+		for (int i = 0; i < string.length(); i++) {
+			if (freq[string.charAt(i) - 'a'] != 0) {
+				if (freq[string.charAt(i) - 'a'] == 1) {
+					index = i;
+					break;
+				}
+
+				freq[string.charAt(i) - 'a'] = 0;
+			}
+		}
+		return index;
+
+	}
+
+}
---
+++ src/javastrings/ProgramToReplaceASubstringInAString.java
@@ -0,0 +1,46 @@
+package javastrings;
+/*
+Program to Replace a Substring in a String | FACE Prep
+
+Program to replace a substring in a string is discussed here. 
+
+The steps for replacing a substring with another string are given below.
+
+
+
+Input:
+
+hi hello
+
+string to be replaced: hi
+
+string to be replaced with: hey
+
+output: hey hello
+
+Algorithm to Replace a substring in a string
+
+
+Input the full string (s1).
+
+Input the substring from the full string (s2).
+
+Input the string to be replaced with the substring (s3).
+
+Find the substring from the full string and 
+
+replace the new substring with the old substring (Find s2 from s1 and replace s1 by s3).
+
+
+Program to Replace a substring in a string
+
+Asked in recruitment drive of Wipro.
+
+*/
+public class ProgramToReplaceASubstringInAString {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
