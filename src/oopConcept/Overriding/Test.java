30b6972863e9e4826e6545d6f28c3035f1830f9a
maainul <mainul080@yahoo.com>
2020-08-28 17:01:22 +0600

    overriding

--- src/oopConcept/Overriding/Test.java
+++ src/oopConcept/Overriding/Test.java
@@ -7,6 +7,12 @@ class P{
 	public void m2(float f) {
 		System.out.println("Parent float call");
 	}
+	
+	public void m3(double d) {
+		System.out.println("parent double");
+	}
+	
+	
 }
 class C extends P{
 	public void m1(int i) {
@@ -17,22 +23,40 @@ class C extends P{
 		System.out.println("Child float call");
 	}
 	
+	
+	
+}
+
+class D extends P{
+	
 }
 
 public class Test {
 
 	public static void main(String[] args) {
 		// parent obj and parent reference
-		P p = new P();
-		p.m1(10);
-		
-		// child object child object
-		C c = new C();
-		c.m1(20);
+//		P p = new P();
+//		p.m1(10);
+//		p.m2(10.5f);
+//		
+//		// child object child object
+//		C c = new C();
+//		c.m1(20);
+//		c.m2(20.5f);
+//		c.m3(30.44d);
 		
 		// child object Parent reference
 		P p1 = new C();
 		p1.m1(30);
+		p1.m2(12.5f);
+		p1.m3(45.5d);
+		// 1st compliler check m3 method in the parent class.
+		// if m3 is not present in the parent class it will 
+		// throw an error
+		// if present then JVM execute the next part 
+		// child class exists m3 then print the result.
+		
+		
 		
 		// parent object child reference
 		//C c1 = new P();
