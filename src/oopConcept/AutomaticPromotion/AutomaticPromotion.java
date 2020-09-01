599e99b8aedb07a077c68bf4ff68338aa3116497
maainul <mainul080@yahoo.com>
2020-09-01 15:40:54 +0600

    automatic promotion

---
+++ src/oopConcept/AutomaticPromotion/AutomaticPromotion.java
@@ -0,0 +1,19 @@
+package oopConcept.AutomaticPromotion;
+
+class Test{
+	public void m2(float i) {
+		System.out.println("int");
+	}
+}
+
+
+public class AutomaticPromotion {
+
+	public static void main(String[] args) {
+		Test obj = new Test();
+		obj.m1(10);
+		
+		
+	}
+
+}
---
+++ src/oopConcept/modifires/_1_Modifires.java
@@ -0,0 +1,27 @@
+package oopConcept.modifires;
+
+class Parent{
+	
+}
+
+abstract class parent2 {
+	
+}
+
+final class parent3{
+	
+}
+
+// default, abstract,final and public is allowed
+
+
+
+
+public class _1_Modifires {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
