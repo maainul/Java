645c64a8f9d60aa359831fd31df7d863bd6cfe69
maainul <mainul080@yahoo.com>
2020-12-01 09:00:49 +0600

    update

--- src/dataStructureAndAlgorithm/Search/Binary/BinarySearch.java
+++ src/dataStructureAndAlgorithm/Search/Binary/BinarySearch.java
@@ -1,45 +1,46 @@
 package dataStructureAndAlgorithm.Search.Binary;
 //Java implementation of iterative Binary Search 
 public class BinarySearch {
+		// Java implementation of iterative Binary Search
+		// Returns index of x if it is present in arr[],
+		// else return -1
+		int binarySearch(int arr[], int x)
+		{
+			int l = 0, r = arr.length - 1;
+			while (l <= r) {
+				int m = l + (r - l) / 2;
 
-	// Driver method to test above
-	public static void main(String args[]) {
+				// Check if x is present at mid
+				if (arr[m] == x)
+					return m;
 
-		int arr[] = { 2, 3, 45, 10, 20, 25, 40 };
+				// If x greater, ignore left half
+				if (arr[m] < x)
+					l = m + 1;
 
-		int x = 0;
+					// If x is smaller, ignore right half
+				else
+					r = m - 1;
+			}
 
-		int result = binarySearch(arr, x);
-		if (result != 0)
-			System.out.println("Element found at " + "index " + result);
-
-		else
-			System.out.println("Element not present");
-	}
-
-	// Returns index of x if it is present in arr[],
-	// else return 0
-	private static int binarySearch(int arr[], int x) {
-		int left = 0;
-		int right = arr.length - 1;
-
-		while (left <= right) {
-			int middle = left + (right - left) / 2;
-
-			// Check if x is present at mid
-			if (arr[middle] == x)
-				return middle;
-			// If x greater, ignore left half
-			else if (arr[middle] < x)
-				left = middle + 1;
-
-			// If x is smaller, ignore right half
-			else
-				right = middle - 1;
+			// if we reach here, then element was
+			// not present
+			return -1;
 		}
-		// if we reach here, then element was
-		// not present
-		return 0;
-	}
 
+		// Driver method to test above
+		public static void main(String args[])
+		{
+			BinarySearch ob = new BinarySearch();
+			int arr[] = { 2, 3, 4, 10, 40 };
+			int n = arr.length;
+			int x = 10;
+			int result = ob.binarySearch(arr, x);
+			if (result == -1)
+				System.out.println("Element not present");
+			else
+				System.out.println("Element found at "
+						+ "index " + result);
+		}
 }
+
--- src/dataStructureAndAlgorithm/Search/Binary/BinarySearchRecursive.java
+++ src/dataStructureAndAlgorithm/Search/Binary/BinarySearchRecursive.java
@@ -2,48 +2,47 @@ package dataStructureAndAlgorithm.Search.Binary;
 
 //Java implementation of iterative Binary Search 
 public class BinarySearchRecursive {
+		// Java implementation of recursive Binary Search
+		// Returns index of x if it is present in arr[l..
+		// r], else return -1
+		int binarySearch(int arr[], int l, int r, int x)
+		{
+			if (r >= l) {
+				int mid = l + (r - l) / 2;
 
-	// Driver method to test above
-	public static void main(String args[]) {
+				// If the element is present at the
+				// middle itself
+				if (arr[mid] == x)
+					return mid;
 
-		int arr[] = { 2, 3, 45, 10, 20, 25, 40 };
+				// If element is smaller than mid, then
+				// it can only be present in left subarray
+				if (arr[mid] > x)
+					return binarySearch(arr, l, mid - 1, x);
 
-		int x = 40;
-		int left = 0;
-		int right = arr.length - 1;
-		int result = binarySearch(arr, left,right,x);
-		if (result != 0)
-			System.out.println("Element found at " + "index " + result);
+				// Else the element can only be present
+				// in right subarray
+				return binarySearch(arr, mid + 1, r, x);
+			}
 
-		else
-			System.out.println("Element not present");
-	}
-
-	// Returns index of x if it is present in arr[],
-	// else return 0
-	private static int binarySearch(int arr[], int left, int right, int x) {
-		if(left > right) {
-			return 0;
+			// We reach here when element is not present
+			// in array
+			return -1;
 		}
 
-		while (left <= right) {
-			int middle = left + (right - left) / 2;
-
-			// Check if x is present at mid
-			if (arr[middle] == x)
-				return middle;
-			// If x greater, ignore left half
-			else if (arr[middle] < x)
-				return binarySearch(arr,middle + 1,right,x);
-
-			// If x is smaller, ignore right half
+		// Driver method to test above
+		public static void main(String args[])
+		{
+			BinarySearchRecursive ob = new BinarySearchRecursive();
+			int arr[] = { 2, 3, 4, 10, 40 };
+			int n = arr.length;
+			int x = 10;
+			int result = ob.binarySearch(arr, 0, n - 1, x);
+			if (result == -1)
+				System.out.println("Element not present");
 			else
-				right = middle - 1;
-			return binarySearch(arr,left,middle-1,x);
+				System.out.println("Element found at index " + result);
 		}
-		// if we reach here, then element was
-		// not present
-		return 0;
 	}
 
-}
+
