aa177c28e8192dbd0346e03166e3f9c35bcce8e8
maainul <mainul080@yahoo.com>
2020-12-08 00:22:48 +0600

    heckerRankProblemSolution

---
+++ src/dataStructureAndAlgorithm/hackerRank/ViralAdvertisement.java
@@ -0,0 +1,21 @@
+package dataStructureAndAlgorithm.hackerRank;
+
+public class ViralAdvertisement {
+    public static void main(String[] args) {
+        int n = 5;
+        adverstise(n);
+    }
+
+    private static void adverstise(int n) {
+        int cum = 0;
+        int people = 5;
+        for (int i = 0; i < n; i++) {
+            int like = (people / 2);
+            int share = (like * 3);
+            cum += like;
+            people = share;
+        }
+        System.out.println(cum);
+    }
+
+}
