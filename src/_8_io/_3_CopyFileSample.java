97e11e7678999aaf2c7645e9bd1a0dd03e267d85
maainul <mainul080@yahoo.com>
2021-02-23 22:29:21 +0600

    Update 2

--- src/_8_io/_3_CopyFileSample.java
+++ src/_8_io/_3_CopyFileSample.java
@@ -7,16 +7,15 @@ public class _3_CopyFileSample {
     public static void copy(File source, File destination) throws IOException {
         try (InputStream in = new FileInputStream(source);
              OutputStream out = new FileOutputStream(destination)) {
-            int b;
-            while ((b = in.read()) != -1) {
+            int b = in.read();
+            while (b  != -1) {
                 out.write(b);
+                b = in.read();
             }
         }
     }
     public static void main(String[] args) throws IOException {
         File source = new File("/home/onik/data/zoo.txt");
         File destination = new File("/home/onik/data/zoooCopy.txt");
-       // File destination = new File("/home/onik/data/zooCopy.txt");
-        copy(source, destination);
     }
 }
